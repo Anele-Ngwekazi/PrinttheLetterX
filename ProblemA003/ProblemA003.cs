e74389d0979b40f93f1dcffb6a833f190c985dc6
Anele Ngwekazi <anele.ngwekazi@gmail.com>
2018-11-26 11:36:08 +0200

    create a grid that prints the letter X

---
+++ ProblemA003/ProblemA003.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ProblemA003
+{
+
+    public class ProblemA003
+    {
+        public void PrinttheLetterX(int sizeOfX)
+        {
+            if (sizeOfX > 9 || sizeOfX < 1)
+            {
+                Console.WriteLine("(size Of X must be between 1-9 (inclusive of 1 and 9).");
+                Console.WriteLine();
+                return;
+            }
+            int count = -sizeOfX;
+            for (int i = 0; i <= 25; i++)
+            {
+                for (int j = 0; j < 79; j++)
+                {
+                
+                    if ((i == 9 || i == 19) && j == 0)
+                    {
+                        Console.Write((i + 1) / 10);
+                    }
+                    else if ((j == 39 - sizeOfX) && (i == 12 - sizeOfX) || (j == 39 + sizeOfX) && (i == 12 + sizeOfX)
+                        || (j == 39 - sizeOfX) && (i == 12 + sizeOfX) || (j == 39 + sizeOfX) && (i == 12 - sizeOfX))
+                    {
+                        Console.Write("$");
+                    }
+                    else if ((j+1)%10==0)
+                    {
+                        Console.Write((j + 1) / 10);
+                    }
+                    else
+                    {
+                        Console.Write("=");
+                    }
+                }
+                if (i == (12 - sizeOfX) && sizeOfX > count)
+                    sizeOfX--;
+                Console.WriteLine();
+            }
+                Console.WriteLine();
+        }
+
+    }
+
+}
---
+++ ProblemA003/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ProblemA003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProblemA003 test = new ProblemA003();
+            // test from 0 to 9
+            // N.B will return line of error
+            for (int i = 0; i < 10; i++)
+            {
+                test.PrinttheLetterX(i);
+            }
+            
+
+            Console.ReadKey();
+        }
+
+    }
+
+}
